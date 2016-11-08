f23843c20a78ec589bc1ea56970990e10f15f116
Cameron Moore <cammoo37@gmail.com>
2016-11-08 11:06:28 -0600

    Code commit test

---
+++ sketch.js
@@ -0,0 +1,19 @@
+// Cameron Moore
+
+var canvasWidth = 500;
+var canvasHeight = 500;
+var midX = canvasWidth/2;       // midpoint X
+var midY = canvasHeight/2;      // midpoint Y
+
+// Color Variables
+var red, green, blue;
+
+function setup()
+{
+
+}
+
+function draw()
+{
+
+}
